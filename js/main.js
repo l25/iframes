da8d05abcf2cc0e9c53b36b76c37a88c06d7d7bd
Valeria S <911025@tut.by>
2021-12-02 09:21:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-let btn = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn');
+const btn2 = document.querySelector('#push_notification_btn');
+
 if(btn){
-  btn.addEventListener('click', () => console.log('iframe'));
+  btn.addEventListener('click', () => console.log('iframe: btn1'));
+}
+if(btn2){
+  btn.addEventListener('click', () => console.log('iframe: btn2'));
 }
