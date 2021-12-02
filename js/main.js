7001d580e21603c2f27e1019a4550c508e3373df
Valeria S <911025@tut.by>
2021-12-02 10:32:40 +0300

    Fix btns

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const btn1 = document.querySelector('#btn');
 const btn2 = document.querySelector('#push_notification_btn');
 
 if(btn1){
-  btn.addEventListener('click', () => console.log('iframe: btn1'));
+  btn1.addEventListener('click', () => console.log('iframe: btn1'));
 }
 if(btn2){
-  btn.addEventListener('click', () => console.log('iframe: btn2'));
+  btn2.addEventListener('click', () => console.log('iframe: btn2'));
 }
