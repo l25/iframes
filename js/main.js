4feceaddf1a0eb9eb0aae48886cad3fa25d09411
Valeria S <911025@tut.by>
2021-12-02 09:23:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const btn1 = document.querySelector('#btn');
 const btn2 = document.querySelector('#push_notification_btn');
 
-if(btn){
+if(btn1){
   btn.addEventListener('click', () => console.log('iframe: btn1'));
 }
 if(btn2){
